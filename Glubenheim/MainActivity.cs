17122634c6a91addf1ad94a12c621a5fd16a26c6
Nicky <nicky.m.k@hotmail.com>
2014-04-15 10:18:54 +0200

    Test of Tcp server / client relation
    Code for making the app a Tcp client added, currently not working, see
    issues for error

--- Glubenheim/MainActivity.cs
+++ Glubenheim/MainActivity.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Sockets;
 using Android.App;
 using Android.Content;
 using Android.Runtime;
@@ -43,6 +44,10 @@ namespace Glubenheim
 				button1.Text = string.Format ("Thanks! {0} clicks.", count++); 
 			};
 
+			button2.Click += delegate {
+				Connect ("127.0.0.1", "Button 2 was clicked");
+			};
+
 			Button showPopupMenu = FindViewById<Button> (Resource.Id.popupButton);
 			showPopupMenu.Click += (s, arg) => {
 				PopupMenu menu = new PopupMenu (this, showPopupMenu);
@@ -63,6 +68,62 @@ namespace Glubenheim
 			Toast toast = Toast.MakeText(this, stringText, ToastLength.Short);
 			toast.SetGravity(GravityFlags.Bottom, 0, 0);
 			toast.Show();
+		}
+
+		// Connect with Tcp server on computer,
+		// while sending a message (server = ipAddress of computer)
+		static void Connect(String server, String message) 
+		{
+			try 
+			{
+				// Create a TcpClient. 
+				// Note, for this client to work you need to have a TcpServer  
+				// connected to the same address as specified by the server, port 
+				// combination.
+				Int32 port = 2814;
+				TcpClient client = new TcpClient(server, port);
+
+				// Translate the passed message into ASCII and store it as a Byte array.
+				Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);         
+
+				// Get a client stream for reading and writing. 
+				//  Stream stream = client.GetStream();
+
+				NetworkStream stream = client.GetStream();
+
+				// Send the message to the connected TcpServer. 
+				stream.Write(data, 0, data.Length);
+
+				Console.WriteLine("Sent: {0}", message);         
+
+				// Receive the TcpServer.response. 
+
+				// Buffer to store the response bytes.
+				data = new Byte[256];
+
+				// String to store the response ASCII representation.
+				String responseData = String.Empty;
+
+				// Read the first batch of the TcpServer response bytes.
+				Int32 bytes = stream.Read(data, 0, data.Length);
+				responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+				Console.WriteLine("Received: {0}", responseData);         
+
+				// Close everything.
+				stream.Close();         
+				client.Close();         
+			} 
+			catch (ArgumentNullException e) 
+			{
+				Console.WriteLine("ArgumentNullException: {0}", e);
+			} 
+			catch (SocketException e) 
+			{
+				Console.WriteLine("SocketException: {0}", e);
+			}
+
+			//Console.WriteLine("\n Press Enter to continue...");
+			//Console.Read();
 		}
 	}
 }

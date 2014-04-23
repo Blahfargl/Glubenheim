1442fc1ba6c8fbaf832b44e97dae6f89121cac32
Annie91 <abech11@student.aau.dk>
2014-04-23 13:36:13 +0200

    more layouts stuff
    worked on layouts

--- Glubenheim/Resources/Resource.designer.cs
+++ Glubenheim/Resources/Resource.designer.cs
@@ -72,32 +72,47 @@ namespace Glubenheim
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f060006
-			public const int item1 = 2131099654;
+			// aapt resource value: 0x7f06000b
+			public const int item1 = 2131099659;
 			
-			// aapt resource value: 0x7f060007
-			public const int item2 = 2131099655;
+			// aapt resource value: 0x7f06000c
+			public const int item2 = 2131099660;
 			
-			// aapt resource value: 0x7f060008
-			public const int item3 = 2131099656;
-			
-			// aapt resource value: 0x7f060000
-			public const int myButton1 = 2131099648;
-			
-			// aapt resource value: 0x7f060002
-			public const int myButton2 = 2131099650;
-			
-			// aapt resource value: 0x7f060003
-			public const int myButton3 = 2131099651;
-			
-			// aapt resource value: 0x7f060004
-			public const int myButton4 = 2131099652;
+			// aapt resource value: 0x7f06000d
+			public const int item3 = 2131099661;
 			
 			// aapt resource value: 0x7f060005
-			public const int myImageButton = 2131099653;
+			public const int myButton1 = 2131099653;
+			
+			// aapt resource value: 0x7f060007
+			public const int myButton2 = 2131099655;
+			
+			// aapt resource value: 0x7f060008
+			public const int myButton3 = 2131099656;
+			
+			// aapt resource value: 0x7f060009
+			public const int myButton4 = 2131099657;
+			
+			// aapt resource value: 0x7f060000
+			public const int myButtonL2_1 = 2131099648;
+			
+			// aapt resource value: 0x7f060002
+			public const int myButtonL2_2 = 2131099650;
+			
+			// aapt resource value: 0x7f060003
+			public const int myButtonL2_3 = 2131099651;
+			
+			// aapt resource value: 0x7f060004
+			public const int myButtonL2_4 = 2131099652;
+			
+			// aapt resource value: 0x7f06000a
+			public const int myImageButton = 2131099658;
+			
+			// aapt resource value: 0x7f060006
+			public const int popupButton = 2131099654;
 			
 			// aapt resource value: 0x7f060001
-			public const int popupButton = 2131099649;
+			public const int popupButtonL2 = 2131099649;
 			
 			static Id()
 			{
@@ -147,11 +162,8 @@ namespace Glubenheim
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int app_name = 2130968577;
-			
 			// aapt resource value: 0x7f040000
-			public const int hello = 2130968576;
+			public const int app_name = 2130968576;
 			
 			static String()
 			{
--- Glubenheim/obj/Debug/android/glubenheim/glubenheim/R.java
+++ Glubenheim/obj/Debug/android/glubenheim/glubenheim/R.java
@@ -18,15 +18,20 @@ public final class R {
         public static final int icon=0x7f020004;
     }
     public static final class id {
-        public static final int item1=0x7f060006;
-        public static final int item2=0x7f060007;
-        public static final int item3=0x7f060008;
-        public static final int myButton1=0x7f060000;
-        public static final int myButton2=0x7f060002;
-        public static final int myButton3=0x7f060003;
-        public static final int myButton4=0x7f060004;
-        public static final int myImageButton=0x7f060005;
-        public static final int popupButton=0x7f060001;
+        public static final int item1=0x7f06000b;
+        public static final int item2=0x7f06000c;
+        public static final int item3=0x7f06000d;
+        public static final int myButton1=0x7f060005;
+        public static final int myButton2=0x7f060007;
+        public static final int myButton3=0x7f060008;
+        public static final int myButton4=0x7f060009;
+        public static final int myButtonL2_1=0x7f060000;
+        public static final int myButtonL2_2=0x7f060002;
+        public static final int myButtonL2_3=0x7f060003;
+        public static final int myButtonL2_4=0x7f060004;
+        public static final int myImageButton=0x7f06000a;
+        public static final int popupButton=0x7f060006;
+        public static final int popupButtonL2=0x7f060001;
     }
     public static final class layout {
         public static final int layout2=0x7f030000;
@@ -36,7 +41,6 @@ public final class R {
         public static final int popup_menu=0x7f050000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f040000;
     }
 }

f5d967253b10e0841814b55ee69e218ccfdf1729
Philip Birk Brisson <philipbrisson@gmail.com>
2014-04-28 15:27:16 +0200

    splash screen quick fix

--- Glubenheim/SplashActivity.cs
+++ Glubenheim/SplashActivity.cs
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,61 +23,4 @@ namespace Glubenheim{
 		}
 	}
 }
-
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-
-namespace Glubenheim{
-	using System.Threading;
-	using Android.App;
-	using Android.OS;
-
-	[Activity(ScreenOrientation = Android.Content.PM.ScreenOrientation.Landscape, Theme = "@style/Theme.Splash", MainLauncher = true, NoHistory = true)]
-	public class SplashActivity : Activity {
-		protected override void OnCreate(Bundle bundle) {
-			base.OnCreate(bundle);
-			Thread.Sleep(2000); // Simulate a long loading process on app startup.
-			StartActivity(typeof(MainActivity));
-		}
-	}
-}
-
->>>>>>> 2160300cd094c3a35b94ba3f2141a56848789e6a
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-
-namespace Glubenheim{
-	using System.Threading;
-	using Android.App;
-	using Android.OS;
-
-	[Activity(ScreenOrientation = Android.Content.PM.ScreenOrientation.Landscape, Theme = "@style/Theme.Splash", MainLauncher = true, NoHistory = true)]
-	public class SplashActivity : Activity {
-		protected override void OnCreate(Bundle bundle) {
-			base.OnCreate(bundle);
-			Thread.Sleep(2000); // Simulate a long loading process on app startup.
-			StartActivity(typeof(MainActivity));
-		}
-	}
-}
-
->>>>>>> 2160300cd094c3a35b94ba3f2141a56848789e6a
 // imageSize = imageSize * 75% of screenSize
